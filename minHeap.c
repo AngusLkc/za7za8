515569c97263b64a8435c68a58f35c235d14233e
cary.zhou <kaichuangluo@gmail.com>
2022-11-19 20:49:10 +0800

    Update minHeap.c

--- minHeap.c
+++ minHeap.c
@@ -12,14 +12,14 @@ typedef struct _tHeap {
 } Heap, *pHeap;
 
 pHeap create(int size) {
-	if (size < 2)
-		return NULL;
+    if (size < 2)
+        return NULL;
     pHeap heap = (pHeap)malloc(sizeof(Heap));
     assert(heap);
     heap->data = (int*)malloc(sizeof(heap->data[0]) * (heap->size + 1));
     assert(heap->data);
-	heap->size = size;
-	heap->count = 1;
+    heap->size = size;
+    heap->count = 1;
     return heap;
 }
 
@@ -33,39 +33,39 @@ void destroy(pHeap heap) {
 
 //上浮
 void shiftUp(pHeap heap, int idx, int val) {
-	int parent = idx >> 1; //父节点在idx/2位置
+    int parent = idx >> 1;
     while (parent >= 1) {
         if (heap->data[parent] > val) {
             heap->data[idx] = heap->data[parent];
-			idx = parent;
-			parent >>= 1; //继续上浮
+            idx = parent;
+            parent >>= 1;
         }
-		else
-			break;
+        else
+            break;
     }
-	heap->data[idx] = val;
+    heap->data[idx] = val;
 }
 
 //下沉
 void shiftDown(pHeap heap, int idx, int val) {
-	int child = idx << 1; //左子节点在idx*2位置
+    int child = idx << 1;
     while (child < heap->count) {
-        if (child + 1 < heap->count)
-            child = heap->data[child] < heap->data[child + 1] ? child : child + 1; //取左右子节点较小值
+        if (child + 1 <= heap->count)
+            child = heap->data[child] < heap->data[child + 1] ? child : child + 1;
         if (val > heap->data[child]) {
-			heap->data[idx] = heap->data[child];
+            heap->data[idx] = heap->data[child];
             idx = child;
-			child <<= 1; //继续下沉
+            child <<= 1;
         }
-		else
-			break;
+        else
+            break;
     }
-	heap->data[idx] = val;
+    heap->data[idx] = val;
 }
 
 void insert(pHeap heap, int val) {
     if (heap->count > heap->size) {
-        heap->size <<= 1; //2倍扩容
+        heap->size <<= 1;
         heap->data = (int*)realloc(heap->data, heap->size + 1);
         assert(heap->data);
     }
